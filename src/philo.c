9142f2faae004345f675d9067c3cda9614f25768
Middle <pourcel.kevin@gmail.com>
2024-10-03 16:46:04 +0200

    Fork_assignement

--- src/philo.c
+++ src/philo.c
@@ -6,19 +6,36 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 14:12:49 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/10/02 16:19:32 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/10/03 16:45:13 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+/**
+ * @brief Initialisation des données importantes.
+ * 
+ * @param data 
+ */
 void	init_data(t_data *data)
 {
-	//int	i;
-	//i = 0;
+	int	i;
+
+	i = 0;
 	data->philos = safe_malloc(sizeof(t_philo) * data->nbr_philo);
 	data->forks = safe_malloc(sizeof(t_fork) * data->nbr_philo);
+	while (i < data->nbr_philo)
+	{
+		pthread_mutex_init(&data->forks[i].fork, NULL);
+		i++;
+	}
 }
+
+/**
+ * @brief  Initialisation des philos + fork assignement
+ * 
+ * @param data 
+ */
 
 void	init_philo(t_data *data)
 {
@@ -32,21 +49,28 @@ void	init_philo(t_data *data)
 		philo->philo_id = i + 1;
 		philo->meals = 0;
 		philo->full = false;
+		fork_assignement(&data->philos[i], data, i);
 		i++;
 	}
 }
 
-void	fork_assignement(t_philo *philo, t_data *data, int philo_id)
+/**
+ * @brief Assignation des fourchettes en fonction de l'id du philo Pair ou impair
+ * 
+ * @param philo 
+ * @param data 
+ * @param id 
+ */
+void	fork_assignement(t_philo *philo, t_data *data, int id)
 {
-	if (philo_id % 2 == 0)
+	if (id % 2 == 0)
 	{
-		philo->left_fork = philo->philo_id + 1 % data->nbr_philo;
-		philo->right_fork = philo->philo_id;
+		philo->left_fork = &data->forks[id];
+		philo->right_fork = &data->forks[(id + 1) % data->nbr_philo];
 	}
 	else
 	{
-		philo->left_fork = philo->philo_id;
-		philo->right_fork = philo_id + 1 % data->nbr_philo;
+		philo->right_fork = &data->forks[id];
+		philo->left_fork = &data->forks[(id + 1) % data->nbr_philo];
 	}
-
 }
